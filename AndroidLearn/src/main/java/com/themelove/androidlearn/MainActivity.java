5ececbb9ec5a67e1f0dd5e2e6b7c36bb770e859c
ThemeLove <lqsthemelove@163.com>
2017-01-04 23:17:39 +0800

    修改了demo

--- AndroidLearn/src/main/java/com/themelove/androidlearn/MainActivity.java
+++ AndroidLearn/src/main/java/com/themelove/androidlearn/MainActivity.java
@@ -1,17 +1,24 @@
 package com.themelove.androidlearn;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private List<VoiceBean> mVoiceBeanList;
+
     private String url="http://cuotiben-mp3.qiniudn.com/XYA07491.mp3";
+    Button play,pause,stop;
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,45 @@ public class MainActivity extends AppCompatActivity {
         for (int i=0;i<=20;i++){
             mVoiceBeanList.add(new VoiceBean(url,VoiceState.noStart));
         }
+
+         player = new MediaPlayer();
+         player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+             @Override
+             public void onPrepared(MediaPlayer mp) {
+                player.start();
+             }
+         });
+
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+
+            }
+        });
+
+        player.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+
+            }
+        });
+
+        player.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+
+                return false;
+            }
+        });
+        try {
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            player.setDataSource(url);
+            player.prepareAsync();
+//            player.prepare();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void initView() {
@@ -38,29 +84,56 @@ public class MainActivity extends AppCompatActivity {
             public void onClickListener(int position) {
 
 //              重新构造数据
-                for (int i=0;i<mVoiceBeanList.size();i++){
+                for (int i = 0; i < mVoiceBeanList.size(); i++) {
                     VoiceBean voiceBean = mVoiceBeanList.get(i);
                     VoiceState voiceState = voiceBean.getVoiceState();
-                    if (i==position){
-                        if (voiceState==VoiceState.noStart){
+                    if (i == position) {
+                        if (voiceState == VoiceState.noStart) {
                             voiceBean.setVoiceState(VoiceState.running);
-                        }else if(voiceState==VoiceState.running){
+                        } else if (voiceState == VoiceState.running) {
                             voiceBean.setVoiceState(VoiceState.pause);
-                        }else if(voiceState==VoiceState.pause){
+                        } else if (voiceState == VoiceState.pause) {
                             voiceBean.setVoiceState(VoiceState.continuePlay);
-                        }else if(voiceState==VoiceState.continuePlay){
+                        } else if (voiceState == VoiceState.continuePlay) {
                             voiceBean.setVoiceState(VoiceState.pause);
                         }
-                    }else{
+                    } else {
                         voiceBean.setVoiceState(VoiceState.noStart);
                     }
                 }
 
-            voiceAdapter.setDataAndRefresh(mVoiceBeanList);
+                voiceAdapter.setDataAndRefresh(mVoiceBeanList);
 
             }
         });
         lv.setAdapter(voiceAdapter);
-    }
 
+
+        //三个测试按钮
+         play = (Button) findViewById(R.id.play);
+         pause = (Button) findViewById(R.id.pause);
+         stop = (Button) findViewById(R.id.stop);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                player.start();
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                player.pause();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                player.stop();
+            }
+        });
+
+    }
 }
